66a0e968a37ee2efead8819249a56c7b52cdcf60
Hindhu04 <108620777+Hindhu04@users.noreply.github.com>
2023-04-30 18:07:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+  numb: 6,
+  question: "Which of the following is a disadvantage of using JavaScript?",
+  answer: "All of the above",
+  options: [
+    "Client-side JavaScript does not allow the reading or writing of files",
+    "JavaScript can not be used for Networking applications because there is no such support available",
+    "JavaScript doesn't have any multithreading or multiprocess capabilities",
+    "All of the above"
+  ]
+},
+{
+numb: 7,
+question: "What will the following code return: Boolean(10 &gt; 9)",
+answer: "true",
+options: [
+    "NaN",
+    "false",
+    "true"
+]
+},
+{
+numb: 8,
+question:"Which operator is used to assign a value to a variable?",
+answer:  "=",
+options: [
+  "*",
+  "-",
+  "=",
+  "x"
+]
+},
+{
+numb: 9,
+question: "How do you declare a JavaScript variable?",
+answer: "var carName;",
+options: [
+  "var carName;",
+  "variable carName;",
+  "v carName;"
+
+]
+},
+{
+numb: 10,
+question: "The external JavaScript file must contain the &lt;script&gt; tag.",
+answer: "False",
+options: [
+"True",
+"False"
+
+]
+},
+
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
